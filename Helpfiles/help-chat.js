dc9f61100405c4b7e44a594df43fc9794134b6f1
semitrivial <alexander@math.ohio-state.edu>
2017-03-01 04:21:40 -0500

    English revisions to help-chat.js (for testing submodule configuration)

--- Helpfiles/help-chat.js
+++ Helpfiles/help-chat.js
@@ -3,11 +3,11 @@ import AddHelp from './AddHelp';
 
 AddHelp('chat', () =>
     <div>
-        The bad news is that everyone wants you dead.  The good news is you
+        The bad news is everyone wants you dead.  The good news is you
         seem to have spontaneously developed telepathy.  You can use the
-        "chat" command to chat with other players currently playing.  For
-        example, if you wanted to chat "Hello world", you would type:{' '}
-        <em>chat Hello world</em>.  Chat messages are visible to all the
-        players currently playing Town Center.
+        "chat" command to chat with other players.  For example, if you
+        wanted to chat "Hello world", you would type: <em>chat Hello
+        world</em>.  Chat messages are visible to all players currently
+        playing Town Center.
     </div>
 );
