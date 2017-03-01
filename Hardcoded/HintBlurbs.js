571a3d12cf6c2009f4ca31d46533300e497dacf5
semitrivial <alexander@math.ohio-state.edu>
2017-03-01 04:31:06 -0500

    Migrate HintBlurbs in from main repo

---
+++ Hardcoded/HintBlurbs.js
@@ -0,0 +1,13 @@
+const HintBlurbs = [
+    'You can disable these hints with "[[hint off]]"',
+
+    'For a full command list, type "[[commands]]"',
+
+    'For help on any command, type "HELP <command>"',
+
+    'You can chat using the chat command (see [[help chat]]).',
+
+    'If things scroll by too fast, try scrolling up.',
+];
+
+export default HintBlurbs;
