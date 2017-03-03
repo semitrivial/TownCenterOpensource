27b85837daa3ba412b4b059d6ca3992847532bf9
semitrivial <alexander@math.ohio-state.edu>
2017-03-02 22:27:21 -0500

    Add hint about /news

--- Hardcoded/HintBlurbs.js
+++ Hardcoded/HintBlurbs.js
@@ -8,6 +8,8 @@ const HintBlurbs = [
     'You can chat using the chat command (see [[help chat]]).',
 
     'If things scroll by too fast, try scrolling up.',
+
+    'To keep track of game development, see [[/news]].',
 ];
 
 export default HintBlurbs;
